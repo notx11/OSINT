1dbb61bf3483661ef7b3207bde236c79ab9abb10
Wayland Morgan <dotwayland@gmail.com>
2016-03-27 18:33:20 -0500

    Create usercheck.py
    Script to check social media platforms for a given pseudonym

---
+++ usercheck.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+
+import requests, getopt, sys
+
+def usage():
+    sys.stderr.write ("Usage: " + sys.argv[0] + " [options]\n")
+    sys.stderr.write ("  -u <userid>           Social Media Handle\n")
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'u:h', ['userid=','help='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    userid = None
+    for o, a in opts:
+        if o in ("-u", "--user"):
+            userid = a
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+
+    websites = ["https://www.instagram.com/", "https://twitter.com/", "http://pastebin.com/u/", "https://www.reddit.com/user/"]
+    username = userid
+
+    for website in websites:
+        url = website + username
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+                print "[*] user exists here: " + url
+        else:
+                print "[!] User NOT FOUND here: " + url
+
+if __name__ == '__main__':
+    main()
